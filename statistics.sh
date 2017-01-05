8c4005f811db3f990009588deeebbb3db96c2da2
K Hong <k.hong@aol.com>
2017-01-05 13:29:20 +0900

    crontab shell script

---
+++ statistics.sh
@@ -0,0 +1,5 @@
+source /home/k/MySites/PyTune3/venv/bin/activate
+/home/k/MySites/PyTune3/venv/bin/python /home/k/MySites/PyTune3/manage.py refresh_feeds --force
+/home/k/MySites/PyTune3/venv/bin/python /home/k/MySites/PyTune3/manage.py collect_stats
+/home/k/MySites/PyTune3/venv/bin/python /home/k/MySites/PyTune3/manage.py index_feeds
+/home/k/MySites/PyTune3/venv/bin/python /home/k/MySites/PyTune3/manage.py collect_feedback
